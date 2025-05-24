eb213d9f45789bb9d663e3dfcef18b8a0fa39717
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-05-24 10:15:12 +0900

    Update test_alu.py

--- python/test_alu.py
+++ python/test_alu.py
@@ -1,17 +1,15 @@
-from unittest import TestCase
+import pytest
 from alu_result import ALU_result
 from alu import ALU
 
-class TestALU(TestCase):
-    def test_test1(self):
-        alu = ALU()
-        alu.set_operand1(10)
-        alu.set_operand2(20)
-        alu.set_opcode("ADD")
+def test_test1():
+    alu = ALU()
+    alu.set_operand1(10)
+    alu.set_operand2(20)
+    alu.set_opcode("ADD")
 
-        ret = ALU_result()
-        alu.enable_signal(ret)
+    ret = ALU_result()
+    alu.enable_signal(ret)
 
-        self.assertEqual(30, ret.get_result())
-        self.assertEqual(0, ret.get_status())
-
+    assert ret.get_result() == 30
+    assert ret.get_status() == 0
